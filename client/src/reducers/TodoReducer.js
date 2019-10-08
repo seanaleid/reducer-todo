64208603b4a1c716176f901b73c02629cc8adf56
seanaleid <seanaleid@gmail.com>
2019-10-08 15:47:25 -0400

    Step 1: create an initial state object and a simple reducer - completed

---
+++ client/src/reducers/TodoReducer.js
@@ -0,0 +1,16 @@
+export const initialState = {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589,
+}
+
+export const todoReducer = (state, action) => {
+    switch(action.type) {
+        case " ":
+            return {
+
+            };
+        default:
+            return state;
+    }
+};
